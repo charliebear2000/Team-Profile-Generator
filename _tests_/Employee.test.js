14880bb7778cf4223ff1b38907f0af5c490eb6a3
Leah <amelia_rawls@ccpsnet.net>
2022-05-02 19:29:09 -0400

    employee passed test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require('../lib/Employee');
+
+console.log(new Employee());
+
+test('creates an employee object', () => {
+   const employee = new Employee('Amy', 123, 'arawls@gmail.com');
+
+   expect(employee.name).toEqual(expect.any(String));
+   expect(employee.id).toEqual(expect.any(Number));
+   expect(employee.email).toEqual(expect.any(String));
+});
+
